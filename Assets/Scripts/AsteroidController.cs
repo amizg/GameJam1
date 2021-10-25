c57159eb24e434e1de94ad0943f5ab388914f4c2
amizg <amizg@umich.edu>
2021-10-25 19:23:40 -0400

    Changed some menus, Added asteroid animation and shuttle sprite

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class AsteroidController : MonoBehaviour
 {
 
-    Rigidbody2D rigidbody2d;
-    Animator animator;
+    Rigidbody2D rb;
+    public Animator animator;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.magnitude > 1000.0f)
+        if (transform.position.magnitude > 100.0f)
         {
             Destroy(gameObject);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Collision with " + other.gameObject);
-        Destroy(gameObject);
-        //animator.SetTrigger("Explode");
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Explode");
+            animator.SetTrigger("Impact");
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
+        Destroy(gameObject, 0.30f);
     }
 }
